576d37f65e16a5604493f72eb817783c78c07f32
liangali <liangali@intel.com>
2021-06-02 19:20:09 +0800

    add gem object creation

--- python/gem.py
+++ python/gem.py
@@ -3,7 +3,8 @@ sys.path.append('../build')
 import pydrm
 
 pydrm.open()
-
+for i in range(10):
+    pydrm.create_gem(4*1024*1024)
 pydrm.close()
 
 print('done')
--- src/pydrm.cpp
+++ src/pydrm.cpp
@@ -3,18 +3,23 @@
 #include <pybind11/stl.h>
 #include <pybind11/numpy.h>
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <fcntl.h>
+#include <inttypes.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
 
 #include <iostream>
 #include <string>
 #include <vector>
 #include <map>
 
-#include <va/va.h>
-#include <va/va_drm.h>
+#include "drm.h"
+#include "i915_drm.h"
 
 namespace py = pybind11;
 
@@ -33,8 +38,24 @@ void drmClose()
     printf("fd closed\n");
 }
 
+int drmCreateGemObject(uint64_t size)
+{
+    uint32_t handle;
+    drm_i915_gem_create gem_create = {};
+    gem_create.size = size;
+    if (ioctl(fd, DRM_IOCTL_I915_GEM_CREATE, &gem_create) == 0) {
+        handle = gem_create.handle;
+        return handle;
+    } else {
+        printf("ERROR: GEM_CREATE failed\n");
+        return -1;
+    }
+}
+
 PYBIND11_MODULE(pydrm, m) {
     m.doc() = "drm python bindings"; 
     m.def("open", &drmOpen, "open display device");
     m.def("close", &drmClose, "close display device");
+
+    m.def("create_gem", &drmCreateGemObject, "create gem object");
 }
